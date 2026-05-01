d120b562bbf5b739817984b73ad625e7d5cae0b5
juliangar-dev <julian.ariel.garciaa@gmail.com>
2026-05-01 18:04:17 -0300

    migracion a PostgreSQL

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///negocios.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///negocios.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
